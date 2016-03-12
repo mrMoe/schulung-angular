b966d7f9f6db75a7b3b6dc906c0d01be424255b7
Moritz Herrmann <moritz_herrmann@gmx.net>
2016-03-13 00:02:05 +0100

    base corrections

--- projects/campus-assessment/config.rb
+++ projects/campus-assessment/config.rb
@@ -5,7 +5,7 @@ require 'compass/import-once/activate'
 http_path = "/"
 css_dir = "app/"
 sass_dir = "app/"
-images_dir = "images"
+images_dir = "images/"
 javascripts_dir = "app/"
 
 # You can select your preferred output style here (can be overridden via the command line):
